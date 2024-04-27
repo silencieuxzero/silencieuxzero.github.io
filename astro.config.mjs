b22198ccd3cb23fce9a6954999c83a7de5c7c19e
silencieuxzero <rain_wind1217@outlook.com>
2024-04-27 22:23:39 +0800

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,8 +24,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://silencieuxzero.github.io/",
+  base: "",
   integrations: [
     tailwind(),
     swup({
